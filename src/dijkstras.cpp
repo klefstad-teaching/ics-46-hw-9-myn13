32002acaf6ce6d349371390a7506d4ff0deec4a2
myn13 <myn13@uci.edu>
2025-03-14 17:55:14 +0000

    9

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -82,7 +82,7 @@ void print_path(const vector<int> & v, int total){
         for (size_t i = 0; i < v.size(); ++i) 
             cout << v[i] << " ";
         cout << endl;
-        cout << "Total cost is " << total << endl;
     }
+    cout << "Total cost is " << total << endl
 }
 
